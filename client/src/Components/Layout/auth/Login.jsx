dc53d275709cb773badf96b88adbd066d57da837
OnShubham <shubhamkharche3005@gmail.com>
2024-01-27 12:44:27 +0530

    login are validation working

--- client/src/Components/Layout/auth/Login.jsx
+++ client/src/Components/Layout/auth/Login.jsx
@@ -1,13 +1,68 @@
-import React from 'react'
+import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
 
 function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    console.log("SUCCESS");
+  };
+
   return (
-    <div>
-        Login
-      
-    </div>
-  )
+    <Fragment>
+      <h1 className="large text-primary">Log in</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Sign in To Your Account
+      </p>
+      <form className="form" onSubmit={onSubmit}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={onChange}
+            required
+          />
+          <small className="form-text">
+            This site uses Gravatar so if you want a profile image, use a
+            Gravatar email
+          </small>
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="6"
+            value={password}
+            onChange={onChange}
+            required
+          />
+        </div>
+
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Login In</Link>
+      </p>
+    </Fragment>
+  );
 }
 
-export default Login
-
+export default Login;
--- client/src/Components/Layout/auth/Register.jsx
+++ client/src/Components/Layout/auth/Register.jsx
@@ -1,12 +1,13 @@
-import React, { Fragment, useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 function Register() {
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    password: '',
-    password2: '',
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
   });
 
   const { name, email, password, password2 } = formData;
@@ -22,9 +23,25 @@ function Register() {
   const onSubmit = async (e) => {
     e.preventDefault();
     if (password !== password2) {
-      console.log('Passwords do not match');
+      console.log("Passwords do not match");
     } else {
-      console.log('Success');
+      const newUser = {
+        name,
+        email,
+        password,
+      };
+      try {
+        const config = {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        };
+        const body = JSON.stringify(newUser);
+        const res = await axios.post("/api/users", body, config);
+        console.log(res.data);
+      } catch (err) {
+        console.error(err.response.data);
+      }
     }
   };
 
