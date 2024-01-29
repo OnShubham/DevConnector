40acfce19370bcd2d6a483c3abe95d126593b20d
OnShubham <shubhamkharche3005@gmail.com>
2024-01-29 13:56:59 +0530

    deploy

--- client/src/store.js
+++ client/src/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from "redux";
 import { thunk } from "redux-thunk"; // Adjusted import statement
 import rootReducer from "./reducer";
-import { composeWithDevTools } from "redux-devtools-extension";
+// import { composeWithDevTools } from "redux-devtools-extension";
 
 const initialState = {};
 
@@ -10,7 +10,7 @@ const middleware = [thunk];
 const store = createStore(
   rootReducer,
   initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
+  (applyMiddleware(...middleware))
 );
 
 export default store;
